54e11ff0597c69d2dec8a95c0a37a8449007948c
riece4020 <cbhtzzf@gmail.com>
2025-10-15 23:56:37 +0900

    the cherno의 Game Engine Series > 31. Vertex Buffer Layout 실습

--- tiny_quality/src/Platform/OpenGL/OpenGLBuffer.h
+++ tiny_quality/src/Platform/OpenGL/OpenGLBuffer.h
@@ -7,12 +7,16 @@ namespace tiny_quality {
 	class OpenGLVertexBuffer : public VertexBuffer {
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	public:
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void UnBind() const;
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
